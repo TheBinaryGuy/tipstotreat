1265cf6dd8c5d6918155f01ed5199839f1855dd2
TheBinaryGuy <hello@thebinary.dev>
2026-07-13 14:17:05 +0530

    Add initial wrangler configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { cloudflare } from '@cloudflare/vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import { devtools } from '@tanstack/devtools-vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
@@ -6,7 +7,13 @@ import { defineConfig } from 'vite';
 
 const config = defineConfig({
     resolve: { tsconfigPaths: true },
-    plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+    plugins: [
+        cloudflare({ viteEnvironment: { name: 'ssr' } }),
+        devtools(),
+        tailwindcss(),
+        tanstackStart(),
+        viteReact(),
+    ],
 });
 
 export default config;
