35ffcce12717b29d892053d6474935b8877552ab
TheBinaryGuy <hello@thebinary.dev>
2026-09-03 03:25:50 +0530

    fix: update import paths for theme provider and styles

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,10 +1,9 @@
+import { ThemeProvider } from '@/components/theme-provider';
+import { getThemeServerFn } from '@/lib/theme';
+import appCss from '@/styles.css?url';
 import { TanStackDevtools } from '@tanstack/react-devtools';
 import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router';
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
-
-import { ThemeProvider } from '../components/theme-provider';
-import { getThemeServerFn } from '../lib/theme';
-import appCss from '../styles.css?url';
 
 export const Route = createRootRoute({
     loader: async () => {
